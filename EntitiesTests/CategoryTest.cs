9013b7a6979ca7a9ce69fe16b3fbf3f8cb69d30f
Maxim Knyazev <cibergamer111@gmail.com>
2021-10-29 17:54:54 +0300

    PS-10: Added entities test project and libraryes.

---
+++ EntitiesTests/CategoryTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+using FluentAssertions;
+using Moq;
+
+namespace EntitiesTests
+{
+    public class CategoryTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+    }
+}
