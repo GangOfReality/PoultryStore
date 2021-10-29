6dffd6ea5042200c31021d38fbd9b5a6a345230b
Maxim Knyazev <cibergamer111@gmail.com>
2021-10-29 18:36:09 +0300

    PS-10: Added BysinessLayerTests.

---
+++ BusinessLayerTests/WatchCatalogUseCaseTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+using FluentAssertions;
+using Moq;
+
+namespace BusinessLayerTests
+{
+    public class WatchCatalogUseCaseTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+    }
+}
